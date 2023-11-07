9744577c3deb33a6863d2624b0c067b0720677fb
Brendan <bgmcmullen@comcast.net>
2023-11-06 20:08:22 -0500

    Added introduction and quesiton #1 is JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('Hello World');
+//Welcome the user
+let userName = prompt('Hello There! What is your name?');
+alert(`Wecome ${userName} I am going to ask you five question about my self. Good Luck!`);
+console.log('I ask you user for their name: ${userName}')
+
+//Question #1
+let userResponse = prompt('Have I been to Luxembourg City twice?').toLowerCase();
+if(userResponse === 'y' || userResponse === 'yes') {
+  alert('Wrong! I\'ve only been there once');
+} else if(userResponse === 'n' || userResponse === 'no') {
+  alert('Correct! I\'ve only been there once');
+}
