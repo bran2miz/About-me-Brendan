7c80d40470d48818a218552b21bafa08aef60fa8
Brendan <bgmcmullen@comcast.net>
2023-11-06 20:30:09 -0500

    modified title format and added bio

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@
 
 //Welcome the user
 let userName = prompt('Hello There! What is your name?');
-alert(`Wecome ${userName} I am going to ask you five question about my self. Good Luck!`);
-console.log('I asked the user for their name: ${userName}')
+alert(`Wecome ${userName} I am going to ask you five questions about my self. Good Luck!`);
+console.log('I asked the user for their name: ${userName}');
 
 //Question #1
 let userResponse = prompt('Have I been to Luxembourg City twice?').toLowerCase();
@@ -12,40 +12,40 @@ if(userResponse === 'y' || userResponse === 'yes') {
 } else if(userResponse === 'n' || userResponse === 'no') {
   alert('Correct! I\'ve only been there once');
 }
-console.log(`The user responded ${userReponse}`)
+console.log(`The user responded ${userResponse}`);
 
 //Question #2
-let userResponse = prompt('Is my favorite TV show Doctor Who?').toLowerCase();
+userResponse = prompt('Is my favorite TV show Doctor Who?').toLowerCase();
 if(userResponse === 'y' || userResponse === 'yes') {
   alert('Wrong! It\'s FireFly');
 } else if(userResponse === 'n' || userResponse === 'no') {
   alert('Correct! It\'s FireFly');
 }
-console.log(`The user responded ${userReponse}`)
+console.log(`The user responded ${userResponse}`);
 
 //Question #3
-let userResponse = prompt('Did I used to live in Texas').toLowerCase();
+userResponse = prompt('Did I used to live in Texas').toLowerCase();
 if(userResponse === 'y' || userResponse === 'yes') {
   alert('Yep! I did my undergrad at Rice University');
 } else if(userResponse === 'n' || userResponse === 'no') {
   alert('Nope! I did my undergrad at Rice University in Houston');
 }
-console.log(`The user responded ${userReponse}`)
+console.log(`The user responded ${userResponse}`);
 
 //Question #4
-let userResponse = prompt('Do I like Oysters on the half shell?').toLowerCase();
+userResponse = prompt('Do I like Oysters on the half shell?').toLowerCase();
 if(userResponse === 'y' || userResponse === 'yes') {
   alert('Yes! I\'m from the Northwest so ofcourse I do.');
 } else if(userResponse === 'n' || userResponse === 'no') {
   alert('Wrong! I\'m from the Northwest so ofcourse I do.');
 }
-console.log(`The user responded ${userReponse}`)
+console.log(`The user responded ${userResponse}`);
 
 //Question #5
-let userResponse = prompt('Did I used to live on a boat?').toLowerCase();
+userResponse = prompt('Did I used to live on a boat?').toLowerCase();
 if(userResponse === 'y' || userResponse === 'yes') {
   alert('No I didn\'t.');
 } else if(userResponse === 'n' || userResponse === 'no') {
   alert('Correct I didn\'t.');
 }
-console.log(`The user responded ${userReponse}`)
+console.log(`The user responded ${userResponse}`);
