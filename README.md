c8d5b58989df47a59138f11ee49d61cf291acdcd
Brendan <bgmcmullen@comcast.net>
2023-11-07 19:21:37 -0500

    completed readme and fixed typos

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 # LAB - 02
 
-# About-me
+## About-me
 
-## Project Name
+This website asks the use five simple questsion about me and display my short biography. The purpose is the give the user a simple and quick way to get to know me
 
-I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
-
-### Author: Student/Group Name
+### Author: Brendan McMullen
 
 ### Links and Resources
 
-* [submission PR](http://xyz.com)
-* Any Links you used as reference
+
 
 ### Lighthouse Accessibility Report Score
 
-* Provide a screenshot of your score after running a Lighthouse Accessibility report.
+![Lighthouse Report](img/About-me-Lighthouse.png)
 
 ### Reflections and Comments
 
-* Consider including the answers to your daily journal and submission questions here
-* This is also a good place to reflect on the tools and resources used and learned
+This is working well and I understand the code. However I am getting two error in the console: 
+
+favicon.ico:1 GET http://127.0.0.1:5500/favicon.ico 404 (Not Found)
+
+and 
+
+127.0.0.1/:1 Uncaught (in promise) Error: A listener indicated an asynchronous response by returning true, but the message channel closed before a response was received
+
+These don't seem to be cause problems but I don't know what is causing this.
--- js/app.js
+++ js/app.js
@@ -3,43 +3,43 @@
 //Welcome the user
 let userName = prompt('Hello There! What is your name?');
 alert(`Wecome ${userName} I am going to ask you five questions about my self. Good Luck!`);
-console.log('I asked the user for their name: ${userName}');
+//console.log('I asked the user for their name: ${userName}');
 
 //Question #1
 let userResponse = prompt('Have I been to Luxembourg City twice?').toLowerCase();
 if(userResponse === 'y' || userResponse === 'yes') {
-  alert('Wrong! I\'ve only been there once');
+  alert('Wrong! I\'ve only been there once.');
 } else if(userResponse === 'n' || userResponse === 'no') {
-  alert('Correct! I\'ve only been there once');
+  alert('Correct! I\'ve only been there once.');
 }
-console.log(`The user responded ${userResponse}`);
+//console.log(`The user responded ${userResponse}`);
 
 //Question #2
 userResponse = prompt('Is my favorite TV show Doctor Who?').toLowerCase();
 if(userResponse === 'y' || userResponse === 'yes') {
-  alert('Wrong! It\'s FireFly');
+  alert('Wrong! It\'s FireFly.');
 } else if(userResponse === 'n' || userResponse === 'no') {
-  alert('Correct! It\'s FireFly');
+  alert('Correct! It\'s FireFly.');
 }
-console.log(`The user responded ${userResponse}`);
+//console.log(`The user responded ${userResponse}`);
 
 //Question #3
 userResponse = prompt('Did I used to live in Texas').toLowerCase();
 if(userResponse === 'y' || userResponse === 'yes') {
-  alert('Yep! I did my undergrad at Rice University');
+  alert('Yep! I did my undergrad at Rice University.');
 } else if(userResponse === 'n' || userResponse === 'no') {
-  alert('Nope! I did my undergrad at Rice University in Houston');
+  alert('Nope! I did my undergrad at Rice University in Houston.');
 }
-console.log(`The user responded ${userResponse}`);
+//console.log(`The user responded ${userResponse}`);
 
 //Question #4
 userResponse = prompt('Do I like Oysters on the half shell?').toLowerCase();
 if(userResponse === 'y' || userResponse === 'yes') {
-  alert('Yes! I\'m from the Northwest so ofcourse I do.');
+  alert('Yes! I\'m from the Northwest so of course I do.');
 } else if(userResponse === 'n' || userResponse === 'no') {
-  alert('Wrong! I\'m from the Northwest so ofcourse I do.');
+  alert('Wrong! I\'m from the Northwest so of course I do.');
 }
-console.log(`The user responded ${userResponse}`);
+//console.log(`The user responded ${userResponse}`);
 
 //Question #5
 userResponse = prompt('Did I used to live on a boat?').toLowerCase();
@@ -48,4 +48,4 @@ if(userResponse === 'y' || userResponse === 'yes') {
 } else if(userResponse === 'n' || userResponse === 'no') {
   alert('Correct I didn\'t.');
 }
-console.log(`The user responded ${userResponse}`);
+//console.log(`The user responded ${userResponse}`);
